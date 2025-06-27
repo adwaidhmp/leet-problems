2b988a5605245886d445b727d831c3b82125bbdc
Adwaidh Mp <adwaidh7727@gmail.com>
2025-06-27 14:19:51 +0530

    Time: 2 ms (74.74%) | Memory: 57.3 MB (17.50%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let i of s) {
+        if (i === '(' || i === '[' || i === '{') {
+            stack.push(i);
+        } else {
+            if (stack.pop() !== map[i]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
